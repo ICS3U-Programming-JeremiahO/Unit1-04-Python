831a894d0334dd3c25a1bd9e45811c8a3a270832
Jeremiah Omoike <jeremiah.omoike@stu.ocsb.ca>
2022-09-19 19:07:52 +0000

    basic_math.py and area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by: Jeremiah omoike
+# Created on: Sep . 16, 2022
+# This program calculates and displays the area and perimeter of a
+# rectangle with length 5cm and width 3cm.
+
+
+def main():
+    print("For a rectangle that has dimensions")
+    print("length 8cm and width 5cm:")
+    print("")
+    print("Area = {} cm^2".format(8 * 5))
+    print("Perimeter = {} cm".format(2 * (8 + 5)))
+
+
+if __name__ == "__main__":
+    main()
--- basic_math.py
+++ basic_math.py
@@ -4,13 +4,15 @@
 # Created on: September 16 2022
 # This program does basic math.
 
+
 def main():
-    print ("9 + 4 = {}". format(9+4))
-    print ("8 - 6 = {}". format(8-6))
-    print ("18 / 2 = {}". format(18/2))
-    print ("5 - 2 / 2 = {}". format(5-2/2))
-    print ("4^3 = {}". format(4**3))
-    print ("2/ 7 = {}". format(2/7))
+    print("9 + 4 = {}".format(9 + 4))
+    print("8 - 6 = {}".format(8 - 6))
+    print("18 / 2 = {}".format(18 / 2))
+    print("5 - 2 / 2 = {}".format(5 - 2 / 2))
+    print("4^3 = {}".format(4**3))
+    print("2/ 7 = {}".format(2 / 7))
+
 
 if __name__ == "__main__":
-  main()
+    main()
